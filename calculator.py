ee4cd8e7d99b5980ba1374dfcdf3828966fec77c
Codings01 <bernardlii4225@gmail.com>
2023-11-07 11:12:58 +0700

    Third Commit

--- calculator.py
+++ calculator.py
@@ -14,34 +14,52 @@ def click(num):
 def clear():
     entry.delete(0, tk.END)
 
-btn_1 = tk.Button(master=frame, text=1, padx=30, pady=10, width=3, command=lambda: myclick(1))
+# def equal():
+
+
+btn_1 = tk.Button(master=frame, text=1, padx=30, pady=10, width=3, command=lambda: click(1))
 btn_1.grid(row=1, column=0, pady=2)
 
-btn_2 = tk.Button(master=frame, text=2, padx=30,pady=10, width=3, command=lambda: myclick(2))
+btn_2 = tk.Button(master=frame, text=2, padx=30,pady=10, width=3, command=lambda: click(2))
 btn_2.grid(row=1, column=1, pady=2)
 
-btn_3 = tk.Button(master=frame, text=3, padx=30, pady=10, width=3, command=lambda: myclick(3))
+btn_3 = tk.Button(master=frame, text=3, padx=30, pady=10, width=3, command=lambda: click(3))
 btn_3.grid(row=1, column=2, pady=2)
 
-btn_4 = tk.Button(master=frame, text=4, padx=30, pady=10, width=3, command=lambda: myclick(4))
+btn_4 = tk.Button(master=frame, text=4, padx=30, pady=10, width=3, command=lambda: click(4))
 btn_4.grid(row=2, column=0, pady=2)
 
-btn_5 = tk.Button(master=frame, text=5, padx=30, pady=10, width=3, command=lambda: myclick(5))
+btn_5 = tk.Button(master=frame, text=5, padx=30, pady=10, width=3, command=lambda: click(5))
 btn_5.grid(row=2, column=1, pady=2)
 
-btn_6 = tk.Button(master=frame, text=6, padx=30, pady=10, width=3, command=lambda: myclick(6))
+btn_6 = tk.Button(master=frame, text=6, padx=30, pady=10, width=3, command=lambda: click(6))
 btn_6.grid(row=2, column=2, pady=2)
 
-btn_7 = tk.Button(master=frame, text=7, padx=30, pady=10, width=3, command=lambda: myclick(7))
+btn_7 = tk.Button(master=frame, text=7, padx=30, pady=10, width=3, command=lambda: click(7))
 btn_7.grid(row=3, column=0, pady=2)
 
-btn_8 = tk.Button(master=frame, text=8, padx=30, pady=10, width=3, command=lambda: myclick(8))
+btn_8 = tk.Button(master=frame, text=8, padx=30, pady=10, width=3, command=lambda: click(8))
 btn_8.grid(row=3, column=1, pady=2)
 
-btn_9 = tk.Button(master=frame, text=9, padx=30, pady=10, width=3, command=lambda: myclick(9))
+btn_9 = tk.Button(master=frame, text=9, padx=30, pady=10, width=3, command=lambda: click(9))
 btn_9.grid(row=3, column=2, pady=2)
 
-btn_0 = tk.Button(master=frame, text=0, padx=30, pady=10, width=3, command=lambda: myclick(0))
+btn_0 = tk.Button(master=frame, text=0, padx=30, pady=10, width=3, command=lambda: click(0))
 btn_0.grid(row=4, column=1, pady=2)
+
+btn_plus = tk.Button(master=frame, text='+', padx=30, pady=10, width=3, command=lambda: click('+'))
+btn_plus.grid(row=5, column=0, pady=2)
+
+btn_minus = tk.Button(master=frame, text='-', padx=30, pady=10, width=3, command=lambda: click('-'))
+btn_plus.grid(row=5, column=1, pady=2)
+
+btn_times = tk.Button(master=frame, text='*', padx=30, pady=10, width=3, command=lambda: click('*'))
+btn_times.grid(row=5, column=2, pady=2)
+
+btn_delete = tk.Button(master=frame, text='<', padx=30, pady=10, width=3, command=lambda: entry.delete(len(entry.get()) - 1))
+btn_delete.grid(row=4, column=2, pady=3)
+
+btn_equal = tk.Button(master=frame, text='=', padx=30, pady=10, width=3, command=eval)
+btn_equal.grid(row=6, column=2, pady=5)
 
 tab.mainloop()
